49ce109392a10119bf73b77f5c0899146e2ed11c
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2026-02-22 11:14:08 -0300

    Creating a constant in the function.

--- Objetose Módulos/Exercicios/salario-liquido.js
+++ Objetose Módulos/Exercicios/salario-liquido.js
@@ -1,3 +1,16 @@
+import * as salaryService from './service/salary-service.js';
+import Employee from './modules/employee.js';
+
 const data = document.getElementById("grossSalaryInout").innerHTML.split("\n");
+
+const employee = new Employee(data[0], parseFloat(data[1]));
+
+const netSalary = salaryService.netSalary(employee);
+
+data.push(netSalary.toFixed(2));
+
+console.log(`Funcionário: ${employee.name}`);
+console.log(`Salário bruto: ${employee.grossSalary}`);
+console.log(`Salário líquido: ${netSalary.toFixed(2)}`);
 
 console.log(data);
