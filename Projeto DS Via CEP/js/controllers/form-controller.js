4eeb5eabbbbc5722952e4b0214d079a3afc44310
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2026-02-27 17:56:25 -0300

    Creating the function (in JS) and naming the tags/divs.

--- Projeto DS Via CEP/js/controllers/form-controller.js
+++ Projeto DS Via CEP/js/controllers/form-controller.js
@@ -1,3 +1,28 @@
+function State() {
+
+    this.btnSave = null;
+    this.btnClear = null;
+
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNumber = null;
+    this.inputCity = null;
+
+    this.errorCep = null;
+    this.errorNumber = null;
+}
+
+const state = new State();
+
 export function init() {
-    constinputCep = document.forms.newAddress.cep;
+    state.inputCep = document.forms.newAddress.cep;
+    state.inputStreet = document.forms.newAddress.street;
+    state.inputNumber = document.forms.newAddress.number;
+    state.inputCity = document.forms.newAddress.city;
+
+    state.btnSave = document.forms.newAddress.btnSave;
+    state.btnClear = document.forms.newAddress.btnClear;
+
+    state.errorCep = document.querySelector(".error-message[data-error='cep']");
+    state.errorNumber = document.querySelector(".error-message[data-error='number']");
 }
